99db633ae02a9c53b5da1e9314805f2497566afe
Emilio <emilioplatzer@gmail.com>
2019-01-08 11:14:07 -0300

    Compila localmente la herencia múltiple de un objeto que contiene un namespace

--- src/server/app-ejemplo-total.ts
+++ src/server/app-ejemplo-total.ts
@@ -1,19 +1,45 @@
 import * as AppApliado from "./app-apilado";
 
-function emergeAppEjemplo(Base:typeof AppApliado){
-// function emergeAppEjemplo<T extends typeof AppApliado>(Base:T){
-    class AppEjemplo extends Base.AppApilado{
+// function emergeAppEjemplo(Base:typeof AppApliado){
+function emergeAppEjemplo<T extends typeof AppApliado>(Base:T){
+    class AppApilado extends Base.AppApilado{
         constructor(...args:any[]){ 
             console.log('eje1',args)
-            super(args);
+            super(...args);
             console.log('eje2',args)
         }
+        get esUno(){ return true };
     }
-    return {...Base, AppApilado:AppEjemplo};
+    return {...Base, AppApilado:AppApilado};
 }
 
-var AppEjemplo=emergeAppEjemplo(AppApliado)
+// function emergeAppEjemplo2(Base:typeof AppApliado){
+function emergeAppEjemplo2<T extends typeof AppApliado>(Base:T){
+    class AppApilado extends Base.AppApilado{
+        constructor(...args:any[]){ 
+            console.log('EJE1',args)
+            super(...args);
+            console.log('EJE2',args)
+        }
+        get esDos(){ return true };
+    }
+    return {...Base, AppApilado:AppApilado};
+}
 
-var app = new AppEjemplo.AppApilado('x');
+var AppEjemplo1=emergeAppEjemplo(AppApliado)
+var AppEjemplo2=emergeAppEjemplo2(AppApliado)
 
-app.start();
+var app1 = new AppEjemplo1.AppApilado('1');
+var app2 = new AppEjemplo2.AppApilado('2');
+
+app1.start();
+console.log('app1',app1.esUno,app1.esDos) // ok: esDos err!
+app2.start();
+console.log('app2',app2.esUno,app2.esDos) // ok: esUno err!
+
+var AppEjemplo12 = emergeAppEjemplo2(emergeAppEjemplo(AppApliado))
+var app12 = new AppEjemplo12.AppApilado('12');
+
+app12.start();
+console.log('app12',app12.esUno,app12.esDos,app12.esTres); // Ok: esTres err!
+
